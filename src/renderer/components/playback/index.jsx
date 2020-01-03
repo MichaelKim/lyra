9b8ce85ed86d81620475734fe9cbcaa67f4a3fbe
Michael Kim <micklepickle98@gmail.com>
2020-01-02 23:52:46 -0500

    Don't autoplay on launch

--- src/renderer/components/playback/index.jsx
+++ src/renderer/components/playback/index.jsx
@@ -1,6 +1,7 @@
 // @flow strict
 
 import * as React from 'react';
+import { connect } from 'react-redux';
 import path from 'path';
 
 import AudioControl from './audio';
@@ -10,138 +11,167 @@ import DownloadQueue from './download';
 import Shuffle from './shuffle';
 import VolumeBar from './volume';
 
-import { useDispatchMap, useSelector } from '../../hooks';
 import { getStreamURL } from '../../yt-util';
+
+import type { Song, StoreState, Dispatch } from '../../types';
 
 import '../../../css/playback.scss';
 
-const mapDispatch = dispatch => {
+type Props = {|
+  +currSong: ?Song,
+  +skipPrevious: () => void,
+  +skipNext: () => void
+|};
+
+type State = {|
+  src: ?string,
+  playing: boolean,
+  progress: number
+|};
+
+class PlaybackBar extends React.Component<Props, State> {
+  state = {
+    src: null,
+    playing: false,
+    progress: 0
+  };
+
+  previousOrStart = () => {
+    if (this.state.progress < 3) {
+      this.props.skipPrevious();
+    } else {
+      this.setState({
+        progress: 0
+      });
+    }
+  };
+
+  onProgress = (time: number) => {
+    this.setState({
+      progress: time
+    });
+  };
+
+  onSeek = (seek: number) => {
+    this.setState(({ progress }) => ({
+      progress: progress + seek
+    }));
+  };
+
+  onTogglePause = () => {
+    this.setState(({ playing }) => ({
+      playing: !playing
+    }));
+  };
+
+  onEnded = () => {
+    this.setState({
+      playing: false
+    });
+    this.props.skipNext();
+  };
+
+  loadSong = async (prevSongID: ?string) => {
+    const { currSong } = this.props;
+    if (currSong == null || prevSongID === currSong.id) {
+      return;
+    }
+
+    // prevSongID == null means called by didMount
+    const replay = prevSongID != null || this.state.playing;
+
+    this.setState({
+      playing: false
+    });
+
+    if (currSong.source === 'YOUTUBE') {
+      const src = await getStreamURL(currSong.id);
+      this.setState({
+        src
+      });
+    } else {
+      this.setState({ src: path.join('file://', currSong.filepath) });
+    }
+
+    if (replay) {
+      this.setState({
+        playing: true
+      });
+    }
+  };
+
+  componentDidMount() {
+    this.loadSong();
+  }
+
+  componentDidUpdate(prevState) {
+    this.loadSong(prevState.currSong?.id);
+  }
+
+  render() {
+    const { currSong, skipNext } = this.props;
+    const { src, playing, progress } = this.state;
+
+    return (
+      <div className='playback-box'>
+        <AudioControl
+          src={src}
+          playing={playing}
+          progress={progress}
+          onProgress={this.onProgress}
+          onEnd={this.onEnded}
+        />
+        <Bar
+          currSong={currSong}
+          progress={progress}
+          onChange={this.onProgress}
+        />
+        <div className='playback-controls'>
+          <div className='playback-left'>
+            {currSong != null && (
+              <>
+                <h3>{currSong.title}</h3>
+                <br />
+                <h5>{currSong.artist}</h5>
+              </>
+            )}
+          </div>
+          <Controls
+            disabled={currSong == null}
+            playing={playing}
+            skipPrevious={this.previousOrStart}
+            skipNext={skipNext}
+            onTogglePause={this.onTogglePause}
+            onSeek={this.onSeek}
+          />
+          <div className='playback-right'>
+            <DownloadQueue />
+            <Shuffle />
+            <VolumeBar />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+const mapState = (state: StoreState) => {
+  const { queue } = state;
+  const { curr } = queue;
+
+  return {
+    currSong: curr != null ? state.songs[curr] ?? queue.cache[curr]?.song : null
+  };
+};
+
+const mapDispatch = (dispatch: Dispatch) => {
   return {
     skipPrevious: () => dispatch({ type: 'SKIP_PREVIOUS' }),
     skipNext: () => dispatch({ type: 'SKIP_NEXT' })
   };
 };
 
-const PlaybackBar = () => {
-  const [src, setSrc] = React.useState<?string>(null);
-  const [playing, setPlaying] = React.useState(true);
-  const [progress, setProgress] = React.useState(0);
-
-  const currSong = useSelector(state => {
-    const { queue } = state;
-    const { curr } = queue;
-
-    return curr != null ? state.songs[curr] ?? queue.cache[curr]?.song : null;
-  });
-
-  const { skipPrevious, skipNext } = useDispatchMap(mapDispatch);
-  const previousOrStart = React.useCallback(() => {
-    if (progress < 3) {
-      skipPrevious();
-    } else {
-      setProgress(0);
-    }
-  }, [skipPrevious, progress]);
-
-  const onProgress = (time: number) => {
-    setProgress(time);
-    // if (
-    //   this.currSong != null &&
-    //   e.currentTarget.currentTime > this.currSong.duration
-    // ) {
-    //   this._onEnded();
-    // }
-  };
-
-  const onSeek = (seek: number) => setProgress(progress => progress + seek);
-
-  const onTogglePause = React.useCallback(() => {
-    setPlaying(playing => !playing);
-  }, []);
-
-  const onEnded = () => {
-    setPlaying(false);
-    skipNext();
-  };
-
-  React.useEffect(() => {
-    if (currSong == null) {
-      return;
-    }
-
-    // Load song data
-    if (currSong.source === 'YOUTUBE') {
-      setPlaying(false);
-      getStreamURL(currSong.id).then(src => {
-        setSrc(src);
-        setPlaying(true);
-      });
-    } else {
-      setSrc(path.join('file://', currSong.filepath));
-      setPlaying(true);
-    }
-
-    // Only load if the song ID is different
-    // The song object can change (e.g. title, artist) while representing the same song
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [currSong?.id]);
-
-  return (
-    <div className='playback-box'>
-      <AudioControl
-        src={src}
-        playing={playing}
-        progress={progress}
-        onProgress={onProgress}
-        onEnd={onEnded}
-      />
-      <Bar currSong={currSong} progress={progress} onChange={onProgress} />
-      <div className='playback-controls'>
-        <div className='playback-left'>
-          {currSong != null && (
-            <>
-              <h3>{currSong.title}</h3>
-              <br />
-              <h5>{currSong.artist}</h5>
-            </>
-          )}
-        </div>
-        <Controls
-          disabled={currSong == null}
-          playing={playing}
-          skipPrevious={previousOrStart}
-          skipNext={skipNext}
-          onTogglePause={onTogglePause}
-          onSeek={onSeek}
-        />
-        <div className='playback-right'>
-          <DownloadQueue />
-          <Shuffle />
-          <VolumeBar />
-        </div>
-      </div>
-      {/* {currSong != null && (
-        <div className='playback-left'>
-          <h3>{currSong.title}</h3>
-          <br />
-          <h5>{currSong.artist}</h5>
-        </div>
-      )}
-      <Controls
-        disabled={currSong == null}
-        playing={playing}
-        skipPrevious={previousOrStart}
-        skipNext={skipNext}
-        onTogglePause={onTogglePause}
-        onSeek={onSeek}
-      />
-      <div className='playback-right'>
-        <DownloadQueue />
-        <Shuffle />
-        <VolumeBar />
-      </div> */}
-    </div>
-  );
-};
-
-export default PlaybackBar;
+export default (connect(
+  mapState,
+  mapDispatch
+)(PlaybackBar): React.ComponentType<{||}>);
